e76f461322d01d0dc6f2390b975c8a9e4a1a8e21
Amanda Diana <amanda.diana@accolade.com>
2021-07-13 14:08:41 +0200

    Add company type schema and connect user to company type

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -7,12 +7,29 @@ const {
 } = require("graphql");
 const axios = require("axios");
 
+const CompanyType = new GraphQLObjectType({
+  name: "CompanyType",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: "UserType",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
