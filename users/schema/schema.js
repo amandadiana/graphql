20620b374e0c43e87e2bd7e241982aaac6ac7523
Amanda Diana <amanda.diana@accolade.com>
2021-07-13 13:20:26 +0200

    Create a schema and add it to the server file

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,5 +1,24 @@
-const { graphql, GraphQLString, GraphQLInt } = require("graphql");
-const { GraphQLObjectType } = graphql;
+const {
+  graphql,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLObjectType,
+} = require("graphql");
+const _ = require("lodash");
+
+const users = [
+  {
+    id: "23",
+    firstName: "Bill",
+    age: 20,
+  },
+  {
+    id: "47",
+    firstName: "Samantha",
+    age: 21,
+  },
+];
 
 const UserType = new GraphQLObjectType({
   name: "UserType",
@@ -8,4 +27,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parantValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
